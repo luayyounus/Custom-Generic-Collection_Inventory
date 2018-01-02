fb623d4ed436546408b23b86e845007e79848029
Luay <luayghazwan@yahoo.com>
2018-01-02 12:39:39 -0800

    Car class implemented

---
+++ EcommerceCarStore/EcommerceCarStore/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcommerceCarStore
+{
+    class Car
+    {
+        private byte Id { get; set; }
+        private string Name { get; set; }
+        private CarType Type { get; set; }
+        private string Color { get; set; }
+        private int Year { get; set; }
+
+        public Car(byte id, string name, CarType carType, string color, int year)
+        {
+            Id = id;
+            Name = name;
+            Type = carType;
+            Color = color;
+            Year = year;
+        }
+
+        public Car(string name, CarType carType)
+        {
+            Name = name;
+            Type = carType;
+        }
+    }
+}
