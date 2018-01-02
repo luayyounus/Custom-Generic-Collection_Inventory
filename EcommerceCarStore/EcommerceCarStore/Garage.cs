f93d0fa2390a73b305b8397dedef4852eb09b561
Luay <luayghazwan@yahoo.com>
2018-01-02 13:14:15 -0800

    Add method implemented

---
+++ EcommerceCarStore/EcommerceCarStore/Garage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcommerceCarStore
+{
+    class Garage<T> : IEnumerable<T>
+    {
+        T[] _cars = new T[10];
+        private int _count = 0;
+
+        public void Add(T car)
+        {
+            if (_count == (_cars.Length / 2))
+            {
+                T[] newArray = new T[_cars.Length * 2];
+
+                for (int i = 0; i < _cars.Length; i++)
+                {
+                    newArray[i] = _cars[i];
+                }
+
+                _cars[_count] = car;
+                _count++;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < _count; i++)
+            {
+                yield return _cars[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- EcommerceCarStore/EcommerceCarStore/Program.cs
+++ EcommerceCarStore/EcommerceCarStore/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EcommerceCarStore
 {
     class Program
     {
+        /// <summary>
+        /// Main entry for the console application
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
