3e7f13ac062a9aa8334dffbb971795773c8f294d
Luay <luayghazwan@yahoo.com>
2018-01-03 00:28:29 -0800

    Xunit first test passing

--- EcommerceStore/XUnitTestEcommerceStore/UnitTest1.cs
+++ EcommerceStore/XUnitTestEcommerceStore/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using EcommerceStore.Classes;
 using Xunit;
 
 namespace XUnitTestEcommerceStore
@@ -6,13 +7,17 @@ namespace XUnitTestEcommerceStore
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Return_Equal_Inventory_Count_When_Adding()
         {
             // Arrange
+            Product product = new Product("Computer", ProductType.Home);
+            Inventory<Product> inventory = new Inventory<Product>();
 
             // Act
+            inventory.Add(product);
 
             // Assert
+            Assert.Equal(1, inventory.Count);
         }
     }
 }
