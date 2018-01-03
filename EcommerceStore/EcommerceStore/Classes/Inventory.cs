680188a00ad2b54ee49002b6ca6570fcfd9f421a
Luay <luayghazwan@yahoo.com>
2018-01-02 20:46:04 -0800

    Inventory class created with Enumerator methods

---
+++ EcommerceStore/EcommerceStore/Classes/Inventory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcommerceStore.Classes
+{
+    class Inventory<T>:IEnumerable<T>
+    {
+        private T[] Items;
+        private int Count;
+
+        public Inventory(int numberOfItems, int count)
+        {
+            Items = new T[numberOfItems];
+            Count = count;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return Items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
