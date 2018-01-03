4252145bf34f8d39dfd37920c14de1726dd325d3
Luay <luayghazwan@yahoo.com>
2018-01-03 00:42:18 -0800

    Second test written

--- EcommerceStore/XUnitTestEcommerceStore/UnitTest1.cs
+++ EcommerceStore/XUnitTestEcommerceStore/UnitTest1.cs
@@ -19,5 +19,20 @@ namespace XUnitTestEcommerceStore
             // Assert
             Assert.Equal(1, inventory.Count);
         }
+
+        [Fact]
+        public void Return_Inventory_Half_Size_When_Removing()
+        {
+            // Arrange
+            Product product = new Product("Computer", ProductType.Home);
+            Inventory<Product> inventory = new Inventory<Product>();
+
+            // Act
+            inventory.Add(product);
+            inventory.Remove(product); // Resizing to half after removing
+
+            // Assert
+            Assert.Equal(5, inventory.Items.Length);
+        }
     }
 }
