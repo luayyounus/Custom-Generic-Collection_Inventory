139dbb341ae0394b0310982564d7c06ce6b01412
Luay <luayghazwan@yahoo.com>
2018-01-02 20:45:13 -0800

    Product Class that holds name and product type

---
+++ EcommerceStore/EcommerceStore/Classes/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcommerceStore.Classes
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public ProductType Type { get; set; }
+    }
+}
